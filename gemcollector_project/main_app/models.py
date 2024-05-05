7a4af4bc15758106ebcc87e872e1d46331169626
linguosity <brandon@linguosity.ai>
2024-05-04 17:05:04 -0700

    update CSS for cards, nav menu bar

--- gemcollector_project/main_app/models.py
+++ gemcollector_project/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Gem(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=250)
+    weight = models.IntegerField(default=0)
+    geotag_location = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'gem_id': self.id})
+
--- gemcollector_project/main_app/urls.py
+++ gemcollector_project/main_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from main_app.views import GemList
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('gems/', views.gems_index, name='index'),
+    path('gems/', GemList.as_view(), name='index'),
+    path('gems/<int:gem_id>/', views.gems_detail,name='detail'),
+    path('gems/create/', views.GemCreate.as_view(), name='gem_create'),
+    path('gems/<int:pk>/update/', views.GemUpdate.as_view(), name='gems_update'),
+    path('gems/<int:pk>/delete/', views.GemDelete.as_view(), name='gems_delete'),
 ]
